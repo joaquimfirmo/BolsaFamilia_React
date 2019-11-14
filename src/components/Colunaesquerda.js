3c7d7433be63480ec0f0150f115012c240b0c28a
Letícia Monteiro <50596098+leehjrs@users.noreply.github.com>
2019-11-14 20:41:29 -0300

    Update Colunaesquerda.js

--- src/components/Colunaesquerda.js
+++ src/components/Colunaesquerda.js
@@ -7,28 +7,25 @@ import 'materialize-css/dist/css/materialize.css';
 
 
 export default class Colunaesquerda extends React.Component {
-
+   
     render() {
 
         return (
             <div>
                 
 
-                    <div className=" col s12 m4 l3   blue-grey lighten-5"  style={{ height: "490px" }}>
+                    <div className=" col s12 m4 l3   blue-grey lighten-5"  style={{ height: "500px" }}>
                         <img className="responsive-img art" src="imagens/BolsaFamilia.png" alt="img"/>
 
                         <hr></hr>
-                        <button id="btncidade" className="btn waves-effect waves-light" type="submit" name="action"
-                            > Cidade </button>
-                        <br></br>
-
-                        <button id="btncpf" className="btn waves-effect waves-light" type="submit" name="action"
-                            >CPF</button>
-
-                        <hr style={{ margintop: "90px" }} />
-
-
-
+                            <button id="btncidade" className="btn waves-effect waves-light" type="submit" name="action"
+                                > Cidade </button>
+                       
+                            <br></br>
+                            <button id="btncpf" className="btn waves-effect waves-light" type="submit" name="action"
+                                >CPF</button>
+                                
+                            <hr style={{ margintop: "90px" }} />
                     </div>
 
                 </div>
@@ -40,7 +37,7 @@ export default class Colunaesquerda extends React.Component {
 
 
 
-
+        
 
     }
 
